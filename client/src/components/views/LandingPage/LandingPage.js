f78dd222dda62bb7cf9c9518ead9d7286ffbb9a7
김준철 <kimjoon889@gamil.com>
2023-01-24 15:09:59 +0900

    5000 번 포트를 이용하는 서버와 3000번 포트를 이용하는 클라이언트 앱은 CORS이슈가 발생한다. 이를 해결해주기 위해서는 Proxy를 황ㄹ용해야한다... https://create-react-app.dev/docs/proxying-api-requests-in-development/ client에 npm install http-proxy-middleware --save 설치를 하고, src/setupProxy.js 파일을 생성하여 해당 파일을 프로토콜에 맞게 작성한다.
    	modified:   client/package-lock.json
    	modified:   client/package.json
    	modified:   client/src/App.js
    	modified:   client/src/components/views/LandingPage/LandingPage.js
    	new file:   client/src/setupProxy.js
    	modified:   server/index.js

--- client/src/components/views/LandingPage/LandingPage.js
+++ client/src/components/views/LandingPage/LandingPage.js
@@ -2,9 +2,7 @@ import React, { useEffect } from "react";
 import axios from "axios";
 function LandingPage() {
     useEffect(() => {
-        axios
-            .get("http://localhost:5000/api/hello")
-            .then((res) => console.log(res.data));
+        axios.get("/api/hello").then((res) => console.log(res.data));
     }, []);
     return <div>LandingPage 랜딩페이지</div>;
 }
